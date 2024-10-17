373815a833075487300c7cb6bbf0bd63f0b0fa3b
raldine <geraldinefoe@gmail.com>
2024-10-17 10:12:37 +0800

    v1

---
+++ src/day06l/App.java
@@ -0,0 +1,27 @@
+package day06l;
+
+
+
+public class App{
+
+    public static void main(String[] args) {
+        Thread th = new Thread(new Runnable() {
+            public void run(){
+
+                for(int i = 0; i < 10; i++){
+                    System.out.println(Thread.currentThread().getName() + " -> " + i);
+                }
+
+
+            }
+        });
+
+    //to run the thread 
+    th.start();
+
+    }
+
+
+
+}
+
